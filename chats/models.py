d87b360443db4318bda3bd0a65b4e9d8339e7fcf
Adewumi Isaac <ienioladewumi@gmail.com>
2021-09-12 10:45:47 +0100

    :necktie: added conversation and message models. Ran migrations

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+import uuid
+from typing import Iterable, Optional
 
-# Create your models here.
+from accounts.models import User
+from django.db import IntegrityError, models
+from django.utils.translation import gettext_lazy as _
+
+
+class Conversation(models.Model):
+    """
+    This model represents a room for chatting, video call and game between two users.
+    """
+
+    id = models.UUIDField(_("conversation id"), primary_key=True, default=uuid.uuid4)
+
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="created_conversations",
+    )
+    invitee = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="joined_conversations",
+        blank=True,
+        null=True,
+    )
+
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+
+    def save(self, *args, **kwargs) -> None:
+        if self.invitee:
+            raise IntegrityError("This conversation is already full!")
+
+        super().save(*args, **kwargs)
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        related_name="messages",
+    )
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+
+    content = models.TextField(_("content"), blank=False, null=False)
+    timestamp = models.DateTimeField(_("timestamp"), auto_now_add=True)
